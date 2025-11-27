956f01e7ffc2cafc6a8b68013dcaec9887faacc4
meghraj <khanatmeghraj@gmail.com>
2025-11-27 12:56:31 +0530

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react"
+import Navbar from './components/Navbar'
+import { Route, Routes } from "react-router-dom"
 
 const App = () =>{
   return(
-    <div>
-
-    </div>
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/movies" element={<Movies/>} />
+    </Routes>
+    </>
   )
 }
 
---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const moviedetail = () => {
+  return (
+    <div>moviedetail</div>
+  )
+}
+
+export default moviedetail
