b6649a133fbdc6aa929194219774f26c83dfcff2
meghraj <khanatmeghraj@gmail.com>
2025-12-27 16:36:32 +0530

    added Authetication using Clerk

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
+
+  const navigate = useNavigate();
 
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
@@ -15,8 +20,8 @@ const Navbar = () => {
       <div
         className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium
 max-md:text-lg z-50 flex flex-col md:flex-row items-center
-max-md:justify-center gap-8 md:px-8 py-3 max-md:h-screen
-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border
+max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen
+min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border
 border-gray-300/20 overflow-hidden transition-[width] duration-300${
           isOpen ? " max-md:w-full" : " max-md:w-0"
         }`}
@@ -38,7 +43,7 @@ border-gray-300/20 overflow-hidden transition-[width] duration-300${
           onClick={() => {
             scrollTo(0, 0), setIsOpen(false);
           }}
-          to="/"
+          to="/movies"
         >
           Movies
         </Link>
@@ -62,7 +67,7 @@ border-gray-300/20 overflow-hidden transition-[width] duration-300${
           onClick={() => {
             scrollTo(0, 0), setIsOpen(false);
           }}
-          to="/"
+          to="/favorite"
         >
           Favorites
         </Link>
@@ -70,9 +75,24 @@ border-gray-300/20 overflow-hidden transition-[width] duration-300${
 
       <div className="flex items-center gap-8">
         <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
-        <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
-          Login
-        </button>
+        {!user ? (
+          <button
+            onClick={openSignIn}
+            className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+          >
+            Login
+          </button>
+        ) : (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="MyBookings"
+                labelIcon={<TicketPlus width={15}/>}
+                onClick={()=> navigate('/MyBookings')}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
       </div>
 
       <MenuIcon
