71cff3fc9317099dae17c036b9bb863943c50cbd
meghraj <khanatmeghraj@gmail.com>
2026-01-18 22:27:27 +0530

    Fixed inngest

--- server/server.js
+++ server/server.js
@@ -11,13 +11,15 @@ const port = 3000;
 
 await connectDB();
 
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 //Middleware
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
 app.get("/", (req, res) => res.send("server is live"));
-app.use("/api/inngest", serve({ client: inngest, functions }));
+// app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(port, () =>
   console.log(`server listening at http://localhost:${port}`)
