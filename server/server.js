bfac0f7a817d715d727639d225f28d14e4457ff4
meghraj <khanatmeghraj@gmail.com>
2026-01-19 20:43:55 +0530

    Fixed inngest

--- server/server.js
+++ server/server.js
@@ -9,20 +9,17 @@ import { inngest, functions } from "./inngest/index.js";
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-app.use(cors());
-
-app.use("/api/inngest", serve({ client: inngest, functions }));
-
 await connectDB();
 
 
 //Middleware
-
+app.use(express.json());
+app.use(cors());
 app.use(clerkMiddleware());
 
+//API routes
 app.get("/", (req, res) => res.send("server is live"));
-// app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(port, () =>
   console.log(`server listening at http://localhost:${port}`)
