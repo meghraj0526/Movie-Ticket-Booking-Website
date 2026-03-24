bf6c9011d6c2c0e69ff2514c58fb9463df31e3a3
meghraj <khanatmeghraj@gmail.com>
2026-03-25 01:33:58 +0530

    booking confirmation added

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
-    user: {type: String, required: true, ref: 'User'},
+    user: {type: String, required: true, ref: 'user'},
     show: {type: String, required: true, ref: 'Show'},
     amount: {type: Number, required: true},
     bookedSeats: {type: Array, required: true},
